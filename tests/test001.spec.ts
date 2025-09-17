216a6d4a1759eef9ec9bcaadca0e0dd8c81ac479
noskill123456 <72763914+noskill123456@users.noreply.github.com>
2025-09-17 17:44:22 +0200

    add test001 200 get response

--- tests/test001.spec.ts
+++ tests/test001.spec.ts
@@ -4,3 +4,8 @@ test('Test 001 - has title Example Domain', async ({ page, request }) => {
   await page.goto('https://example.com');
   await expect(page).toHaveTitle(/Example Domain/);
 });
+
+test('Test 002 - page return 200', async ({request})  => {
+  const response = await request.get('https://example.com');
+  expect(response.status()).toBe(200);
+});
